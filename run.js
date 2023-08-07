2195fb81e4bbb2f87b66fb9b1cbd772332a56f7a
Shakhzod Bobolov <shohbobolov98@gmail.com>
2023-08-07 11:08:28 +0500

    The data of the uzbek words.

--- run.js
+++ run.js
@@ -1,10 +1,8 @@
 const fs = require("fs");
 
-// const words70k = JSON.parse(fs.readFileSync("./uzbek_70k.json", "utf-8"));
 const izohli_lugat = JSON.parse(
 	fs.readFileSync("./izohli_lug'at.json", "utf-8")
 );
-
 const words_data = JSON.parse(fs.readFileSync("./words_data.json", "utf-8"));
 
 console.log(
